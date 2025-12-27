071a44e6f7e5c79f1080539a8128f5b877569d22
kiran <kiranpar321@gmail.com>
2025-12-26 21:29:08 -0800

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import Work from './pages/Work.jsx'
+import Work from './pages/work.jsx'
 import Contact from './pages/Contact.jsx'
 const App = () => {
   return (
     <div>
+      <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/about' element={<About/>}/>
+        <Route path='/work' element={<Work/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
       
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import prince from "../assets/avatar.webp";
+import { faHome, faUser, faBriefcase, faEnvelope, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
 
 const Home = () => {
+
+    const[isNavOpen,setIsNavOpen]=useState(false);
+
+    const taggleNav=()=>{
+        setIsNavOpen(!isNavOpen);
+    }
+
   return (
-    <div>Home</div>
+    <div>
+        <main className='home'>
+            
+            <header>
+                <div className="logo">
+        <h1>Prince</h1>
+                </div>
+       <nav className={isNavOpen ? 'nav-menu active' : 'nav-menu'}>
+        <ul>
+            <li>
+                <Link to="/" className='nav-link' onClick={()=>{setIsNavOpen(false)}}> <FontAwesomeIcon icon={faHome} /> Home</Link>
+            </li>
+            <li>
+                <Link to="/about" className='nav-link'onClick={()=>{setIsNavOpen(false)}}> <FontAwesomeIcon icon={faUser} />About</Link>
+            </li>
+            <li>
+                <Link to="/work" className='nav-link' onClick={()=>{setIsNavOpen(false)}} ><FontAwesomeIcon icon={faBriefcase} />Work</Link>
+            </li>
+            <li>
+                <Link to="/contact" className='nav-link' onClick={()=>{setIsNavOpen(false)}}><FontAwesomeIcon icon={faEnvelope} />Contact</Link>
+            </li>
+
+            
+          
+        </ul>
+       </nav>
+       <div className="hambur" onClick={taggleNav}>
+                <FontAwesomeIcon icon={isNavOpen ? faXmark: faBars} />
+            </div>
+       </header>
+
+       <section className='hero-section'>
+        <div className="hero-text">
+        <h2>Hi I'm Prince </h2>
+            <h3>Editor | Content Creator</h3>
+            <p>Prince is a meticulous and creative editor with a keen eye for detail and a passion for storytelling. With</p>
+            <p>extensive experience in refining content for clarity, flow, and impact, he ensures every piece resonates with</p>
+            <p>its audience. Whether it's polishing articles, scripts, or digital content, Prince brings precision,</p>
+            <p>creativity, and a professional touch to every project he undertakes.</p>
+        </div>
+        <div className="hero-image">
+            <img src={prince} alt="Hero" />
+        </div>
+       </section>
+
+</main>
+    </div>
   )
 }
 
